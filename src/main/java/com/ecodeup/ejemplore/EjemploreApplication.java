e93750a664e01ff8db1592d4e1b70afcad6e1af2
David Chavez <jose_17298@hotmail.com>
2024-12-22 02:29:53 -0500

    primer commit

---
+++ src/main/java/com/ecodeup/ejemplore/EjemploreApplication.java
@@ -0,0 +1,13 @@
+package com.ecodeup.ejemplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecodeup/ejemplore/EjemploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodeup.ejemplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
